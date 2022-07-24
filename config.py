45b99c0d998fa7f6dcae0717dc0636cbbac56397
Block Party <109920859+TheBlockParty@users.noreply.github.com>
2022-07-24 12:58:12 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# Details of asset creation.
+asset_details = {
+	"asset_name": "Block Party",
+	"unit_name": "BP",
+	"total": 1000000000,
+	"decimals": 2,
+	"default_frozen": False,
+	"manager": creator_address,
+	"reserve": creator_address,
+	"freeze": "",
+	"clawback": "",
+	"url": "BP.png",
+	"metadata_hash": b'21IzslEhVytCQQWw8AG40AZ7SmxwHtamONQ5V1k78L0='
+}
